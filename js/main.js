a8d96c4791218df7bfc885e46c4a590421542ada
israelcesar <israelcesars@gmail.com>
2021-09-09 16:36:41 -0300

    Conclusão do módulo 1 de JS

---
+++ js/main.js
@@ -0,0 +1,56 @@
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+alert(soma(5, 10))
+
+/*
+var d = new Date()
+alert(d.getUTCFullYear())
+*/
+
+/*
+for(count = 0; count <= 5; count++){
+    console.log(count)
+}
+*/
+
+/*
+var count = 0;
+while(count <= 5){
+    console.log(count)
+    count++;
+}
+*/
+
+//var idade = prompt("Qual sua idade?")
+//if(idade >= 18) {
+//    alert("maior de idade")
+//}else{
+//    alert("menor de idade")
+//}
+
+//var fruta = {nome: "maçã", cor:"vermelha"}
+//console.log(fruta.nome)
+
+//var frutas = [{nome: "Maçã", cor: "vermelha"}, {nome: "Uva", cor: "verde"}]
+//console.log(frutas[1].nome)
+
+//var lista = ["maçã", "pera", "laranja"]
+//console.log(lista[1])
+//alert(lista[1])
+//lista.push("uva")
+//lista.pop()
+//console.log(lista.length)
+//console.log(lista.reverse())
+//console.log(lista.toString())
+//console.log(lista.join(" - "))
+//console.log(lista)
+
+//var nome = "Israel César";
+//var idade = 37;
+//var idade2 = 10;
+//var frase = "O Japão é o melhor time do mundo."
+//alert (nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"))
